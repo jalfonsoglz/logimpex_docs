1c56efc3655da550672c7cfb2bd184b04bff7881
DesarrolloPtree <alfonsoglzslz@gmail.com>
2022-03-20 08:55:14 -0500

    Update docs_tags.py

--- models/docs_tags.py
+++ models/docs_tags.py
@@ -4,7 +4,6 @@ from odoo import fields, models, api
 class DocsTags(models.Model):
 	_name = 'logimpex.docs.tags'
 	_description = 'Etiquetado de Documentos'
-	_order = id
 
 	name = fields.Char()
 	color = fields.Integer()
