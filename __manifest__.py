9b38a0c5a89093acf3f2b2810c0a90e5854e7b79
DesarrolloPtree <alfonsoglzslz@gmail.com>
2022-04-02 22:30:08 -0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,7 @@
     'category': 'Customizations',
     'version': '15.0.0.0.1',
     'license': "AGPL-3",
-    'sequence': "-99",
+    'sequence': "-97",
     'depends': [
         'base',
         'mail',
